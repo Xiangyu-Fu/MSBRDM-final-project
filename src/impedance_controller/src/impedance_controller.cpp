cb715f6b654ee9d7cbe3d96c6e4b163f90115415
Yanbing <yanbing.liu@tum.de>
2024-02-25 21:20:16 +0100

    new ur model

--- src/impedance_controller/src/impedance_controller.cpp
+++ src/impedance_controller/src/impedance_controller.cpp
@@ -65,7 +65,7 @@ namespace tum_ics_ur_robot_lli
       running_time_ = 0.0;
 
       // get the current joint state
-      auto T_ef_0 = model_.T_ef_0(joint_state_.q);
+      auto T_ef_0 = model_.Tef_0(joint_state_.q);
       // std::cout << "T_ef_0: " << T_ef_0.translation().transpose() << std::endl;
 
       ee_start_.pos().linear() = T_ef_0.translation();
@@ -189,7 +189,7 @@ namespace tum_ics_ur_robot_lli
         m_error = true;
         return false;  
       }
-      theta_ = model_.parameterInitalGuess();
+      theta_ = model_.Theta_function();
 
       ros::param::get(ns_ + "/learning_rate", gamma_);
       if (!(gamma_ > 0))
@@ -286,7 +286,7 @@ namespace tum_ics_ur_robot_lli
       {
         q_start_ = state.q;
         ROS_WARN_STREAM("START [DEG]: \n" << q_start_.transpose());
-        ROS_WARN_STREAM("START [CART]: \n" << model_.T_ef_0(state.q).linear());
+        ROS_WARN_STREAM("START [CART]: \n" << model_.Tef_0(state.q).linear());
 
         // Init time
         time_prev_ = ros::Time::now();
@@ -296,7 +296,7 @@ namespace tum_ics_ur_robot_lli
         is_first_iter_ = false;
 
         // Init model
-        theta_ = model_.parameterInitalGuess();
+        theta_ = model_.Theta_function();
 
         // Init error
         joint_error_ = Vector6d::Zero();
@@ -315,7 +315,7 @@ namespace tum_ics_ur_robot_lli
       time_prev_ = time_cur;
       running_time_ += dt;
       joint_state_ = state;
-      auto X_ee = model_.T_ef_0(state.q);
+      auto X_ee = model_.Tef_0(state.q);
 
       // publish  data
       {
@@ -393,7 +393,7 @@ namespace tum_ics_ur_robot_lli
         // torque
         Vector6d Sq = state.qp - q_ref.qp;
         // Compute regressor for cartesian control
-        const auto& Yr = model_.regressor(state.q, state.qp, q_ref.qp, q_ref.qpp);
+        const auto& Yr = model_.Yr_function(state.q, state.qp, q_ref.qp, q_ref.qpp);
         theta_ -= gamma_ * Yr.transpose() * Sq * dt;
         tau = -Kd_ * Sq + Yr * theta_;
         return tau;
@@ -428,7 +428,7 @@ namespace tum_ics_ur_robot_lli
         q_ref.qp = q_desired_[1] - Kp_ * (state.q - q_desired_[0])- Ki_ * joint_error_;
         q_ref.qpp = q_desired_[2] - Kp_ * (state.qp - q_desired_[1])- Ki_ * joint_dot_error_;
         // torque
-        const auto& Yr = model_.regressor(state.q, state.qp, q_ref.qp, q_ref.qpp);
+        const auto& Yr = model_.Yr_function(state.q, state.qp, q_ref.qp, q_ref.qpp);
         Vector6d Sq = state.qp - q_ref.qp;
         tau = -Kd_ * Sq + Yr * theta_;
         return tau;
@@ -458,9 +458,9 @@ namespace tum_ics_ur_robot_lli
         x_desired_.acc() = low_pass_factor_ * x_desired_.acc() + (1 - low_pass_factor_) * x_desired_cur.acc();
 
         // get model
-        auto X_ee = model_.T_ef_0(state.q);
-        auto Jef = model_.J_ef_0(state.q);
-        auto Jef_dot = model_.Jp_ef_0(state.q, state.qp);
+        auto X_ee = model_.Tef_0(state.q);
+        auto Jef = model_.Jef_0(state.q);
+        auto Jef_dot = model_.Jef_0_dot(state.q, state.qp);
 
         // current cartesian state
         cc::CartesianState x_current;
@@ -486,7 +486,7 @@ namespace tum_ics_ur_robot_lli
         Qrpp = Jef_pinv * (Xr.acc() - Jef_dot * state.qp);
 
         Vector6d Sq = state.qp - Qrp;
-        const auto& Yr = model_.regressor(state.q, state.qp, Qrp, Qrpp);
+        const auto& Yr = model_.Yr_function(state.q, state.qp, Qrp, Qrpp);
         theta_ -= gamma_ * Yr.transpose() * Sq * dt;
         Vector6d tau_tracking = -Kd_cart_ * Sq + Yr * theta_;
 
@@ -572,7 +572,7 @@ namespace tum_ics_ur_robot_lli
       Vector6d tau_red_i;
       double d;
       double d_min = 10e-5;
-      T_i_0 = model_.T_j_0(state.q, j);
+      T_i_0 = model_.Ti_0(state.q, j);
       d = (X_red_ - T_i_0.position()).norm();  
       ROS_INFO_STREAM( " Distance Ball from joint j " << j << "  " << d);
       if(d < d_min)
@@ -590,7 +590,7 @@ namespace tum_ics_ur_robot_lli
         F_red_i.y() = current_spingK.y() /  (X_red_ - T_i_0.position()).y();
         F_red_i.z() = current_spingK.z() /  (X_red_ - T_i_0.position()).z();
 
-        tau_red_i = model_.J_j_0(state.q,j).block(0, 0, 2, 5).transpose() * F_red_i;
+        tau_red_i = model_.Ji_0(state.q,j).block(0, 0, 2, 5).transpose() * F_red_i;
       }
       else    // else we can set everything to zero 
           tau_red_i.setZero(); 
--- src/ur_model/src/applications/test_ur10_model_node.cpp
+++ src/ur_model/src/applications/test_ur10_model_node.cpp
@@ -7,7 +7,7 @@ int main(int argc, char **argv)
   ros::NodeHandle nh("~");
 
   // initalize the model (only done once!)
-  ur::URModel model("ur10_model");
+  ur_model_namespace::URModel model("ur10_model");
 
   // load the intial parameters from the parameter server
   if(!model.initRequest(nh))
@@ -19,32 +19,32 @@ int main(int argc, char **argv)
   //----------------------------------------------------------------------------
   // test some function calls
 
-  cc::VectorDof q, qP, qrP, qrPP;
+  cc::VectorDof q, qp, qrp, qrpp;
   q = M_PI*cc::VectorDof::Random();
-  qP.setZero();
-  qrP.setZero();
-  qrPP.setZero();
+  qp.setZero();
+  qrp.setZero();
+  qrpp.setZero();
   
   // Initalize the robot regressor matrix
-  ur::URModel::Regressor Y = model.regressor(q, qP, qrP, qrPP);
+  ur_model_namespace::URModel::Regressor_Yr Yr = model.Yr_function(q, qp, qrp, qrpp);
   
   // Initalize the robots parameter vector
-  ur::URModel::Parameters th = model.parameterInitalGuess(); 
+  ur_model_namespace::URModel::Regressor_Theta Theta = model.Theta_function(); 
 
   // get the end-effector transformation wrt 0 frame
-  Eigen::Affine3d T_ef_0 = model.T_ef_0(q);
+  Eigen::Affine3d Tef_0 = model.Tef_0(q);
 
   // get the end-effector jacobain wrt 0 frame
-  Eigen::Matrix<double,6,6> J_ef_0 = model.J_ef_0(q);
+  Eigen::Matrix<double,6,6> Jef_0 = model.Jef_0(q);
 
   //----------------------------------------------------------------------------
   // print some stuff
 
   ROS_WARN_STREAM("q=\n" << q.transpose() << "\n");
-  ROS_WARN_STREAM("T_ef_0=\n" << T_ef_0.matrix() << "\n");
-  ROS_WARN_STREAM("J_ef_0=\n" << J_ef_0 << "\n");
-  ROS_WARN_STREAM("th=\n" << th.transpose() << "\n");
-  ROS_WARN_STREAM("Y=\n" << Y << "\n");
+  ROS_WARN_STREAM("Tef_0=\n" << Tef_0.matrix() << "\n");
+  ROS_WARN_STREAM("Jef_0=\n" << Jef_0 << "\n");
+  ROS_WARN_STREAM("Theta=\n" << Theta.transpose() << "\n");
+  ROS_WARN_STREAM("Yr=\n" << Yr << "\n");
 
   return 0;
 }
